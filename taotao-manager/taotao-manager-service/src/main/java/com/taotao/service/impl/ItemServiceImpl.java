22589575363d8803c78f44b8763fba8181c56677
cyb-xh <904664794@qq.com>
2020-05-27 09:57:15 +0800

    first commit

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -0,0 +1,152 @@
+package com.taotao.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.IDUtils;
+import com.taotao.jedis.service.JedisClient;
+import com.taotao.mapper.TbItemDescMapper;
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemDesc;
+import com.taotao.pojo.TbItemExample;
+import com.taotao.service.ItemService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.jms.*;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+    @Autowired
+    private TbItemMapper itemMapper;
+    @Autowired
+    private TbItemDescMapper itemDescMapper;
+    @Autowired
+    private JmsTemplate jmsTemplate;
+    @Resource(name = "itemAddTopic")
+    private Destination destination;
+    @Autowired
+    private JedisClient jedisClient;
+    @Value("${ITEM_INFO}")
+    private String ITEM_INFO;
+    @Value("${ITEM_EXPIRE}")
+    private Integer ITEM_EXPIRE;
+
+    @Override
+    public TbItem getItemById(long itemId) {
+        //查询数据库之前先查询缓存
+        try {
+            String json = jedisClient.get(ITEM_INFO+":"+itemId+":BASE");
+            if(!StringUtils.isBlank(json)){
+                //把json转换成对象
+                return JSON.parseObject(json, TbItem.class);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //缓存中没有查询数据库
+        TbItem tbItem=itemMapper.selectByPrimaryKey(itemId);
+        //把查询结果添加到缓存
+
+        try {
+            //把查询结果添加到缓存
+            jedisClient.set(ITEM_INFO+":"+itemId+":BASE", JSON.toJSONString(tbItem));
+            //设置过期时间，提高缓存的利用率
+            jedisClient.expire(ITEM_INFO+":"+itemId+":BASE", ITEM_EXPIRE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tbItem;
+    }
+
+    @Override
+    public EasyUIDataGridResult getItemList(int page, int rows) {
+        //设置分类信息
+        PageHelper.startPage(page, rows);
+        //执行查询
+        TbItemExample example=new TbItemExample();
+        List<TbItem> list=itemMapper.selectByExample(example);
+        //取查询结果
+        PageInfo<TbItem> pageInfo=new PageInfo<>(list);
+        EasyUIDataGridResult result=new EasyUIDataGridResult();
+        result.setRows(list);
+        result.setTotal(pageInfo.getTotal());
+        //返回结果
+        return result;
+    }
+    @Override
+    public TaotaoResult createItem(TbItem tbItem, String desc){
+        //生成商品ID
+        final long itemId = IDUtils.genItemId();
+        //补全item的属性
+        tbItem.setId(itemId);
+        //商品状态，1-正常，2-下架，3-删除
+        tbItem.setStatus(((byte) 1));
+        tbItem.setCreated(new Date());
+        tbItem.setUpdated(new Date());
+        itemMapper.insert(tbItem);
+        //添加商品描述
+        insertItemDesc(itemId, desc);
+        //向Activemq发送商品添加消息
+        jmsTemplate.send(destination, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                //发送商品id
+                TextMessage textMessage = session.createTextMessage(itemId+"");
+                return textMessage;
+            }
+        });
+
+        return TaotaoResult.ok();
+    }
+
+    //添加商品描述
+    private void insertItemDesc(long itemId,String desc){
+        //创建一个商品描述表对应的pojo
+        TbItemDesc itemDesc = new TbItemDesc();
+        //补全pojo的属性
+        itemDesc.setItemId(itemId);
+        itemDesc.setItemDesc(desc);
+        itemDesc.setCreated(new Date());
+        itemDesc.setUpdated(new Date());
+        //向商品描述表插入数据
+        itemDescMapper.insert(itemDesc);
+    }
+
+    @Override
+    public TbItemDesc getItemDescById(long itemId) {
+        //查询数据库之前先查询缓存
+        try {
+            String json = jedisClient.get(ITEM_INFO+":"+itemId+":DESC");
+            if(!StringUtils.isBlank(json)){
+                //把json转换成对象
+                return JSON.parseObject(json, TbItemDesc.class);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        TbItemDesc tbItemDesc = itemDescMapper.selectByPrimaryKey(itemId);
+        //把查询结果添加到缓存
+        try {
+            //把查询结果添加到缓存
+            jedisClient.set(ITEM_INFO+":"+itemId+":DESC", JSON.toJSONString(tbItemDesc));
+            //设置过期时间，提高缓存的利用率
+            jedisClient.expire(ITEM_INFO+":"+itemId+":DESC", ITEM_EXPIRE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tbItemDesc;
+    }
+
+}
